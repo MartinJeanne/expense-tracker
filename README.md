f38931d9cf3acbab681296080f6abaa9b1d1564d
MartinJeanne <martinjeanne.dev@gmail.com>
2024-09-02 18:36:07 +0200

    Making project CLI executable from anywhere!

--- README.md
+++ README.md
@@ -5,12 +5,17 @@ Project to train back-end skills. Made with Node.js with TypeScript.
 Roadmap project link: https://roadmap.sh/projects/expense-tracker
 
 ## Run
-With Node.js on your PC, do:
+With Node.js on your PC, clone the project and then do:
 ```
 npm i
 ```
 
 ```
-npm start
+npm link
+```
+You can now use the following command wherever you want: `expense-tracker`  
+If you want to undo that:
+```
+npm unlink --global
 ```
 ## How to use
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello!');
+#! /usr/bin/env node
+console.log("Hello World! Wassup?");
+console.log(process.argv);
+
+if (process.argv.length === 2) {
+    console.error('Expected at least one argument!');
+    process.exit(1);
+}
+
+// Check to see if the -f argument is present
+const flag = (
+    process.argv.indexOf('-f') > -1 ? 'Flag is present.' : 'Flag is not present.'
+);
+
+// Checks for --custom and if it has a value
+const customIndex = process.argv.indexOf('--custom');
+let customValue;
+
+if (customIndex > -1) {
+    // Retrieve the value after --custom
+    customValue = process.argv[customIndex + 1];
+}
+
+const custom = (customValue || 'Default');
+
+console.log('Flag:', `${flag}`);
+console.log('Custom:', `${custom}`);
